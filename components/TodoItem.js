671c651efeae644b1d4279d1a21757198c7034b6
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2022-01-10 14:44:56 +0600

    flexbox and icons

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function TodoItem({item,pressHandler}) {
     return (
-    <TouchableOpacity onPress={()=>pressHandler(item.key)}>
-       <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={20} color="#333" />
+                <Text style={styles.itemText}>{item.text}</Text>
+            </View>
     </TouchableOpacity>
   );
 }
@@ -16,5 +20,9 @@ const styles = StyleSheet.create({
         borderWidth:1,
         borderStyle: 'dashed',
         borderRadius: 10,
+        flexDirection:'row'
+    },
+    itemText: {
+        marginLeft:10
     }
 });
