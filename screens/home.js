32e8ab24d511488b540452436a0462db489d67a5
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2022-01-14 11:59:20 +0600

    navigating around

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { GlobalStyles } from "../styles/global";
 
-export default function Home() {
+export default function Home({navigation}) {
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+        // navigation.push('ReviewDetails')
+
+    }
     return (
         <View style={GlobalStyles.container}>
             <Text style={GlobalStyles.titleText}>Home screen</Text>
+            <Button title="Go to review" onPress={pressHandler}/>
         </View>
     )
 }
