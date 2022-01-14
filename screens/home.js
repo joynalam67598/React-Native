abf848f7a68e7c612da5a6c74a7c788d51bc64f1
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2022-01-14 14:56:41 +0600

    passing data between screens

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,28 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React,{useState} from "react";
+import { View, Text, Button,FlatList,TouchableOpacity } from "react-native";
 import { GlobalStyles } from "../styles/global";
 
-export default function Home({navigation}) {
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-        // navigation.push('ReviewDetails')
-
-    }
+export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        {title: 'Zelda, Breath of fresh year', rating: 5, body:'loream' , key:1},
+        {title: 'Breath of fresh year', rating: 4, body:'fresh year' , key:2},
+        {title: 'Breath of fresh ', rating: 3, body:'Breath of  year' , key:3}
+       
+    ])
+    
     return (
         <View style={GlobalStyles.container}>
-            <Text style={GlobalStyles.titleText}>Home screen</Text>
-            <Button title="Go to review" onPress={pressHandler}/>
+            <FlatList
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        onPress={() =>
+                            navigation.navigate('ReviewDetails', item)
+                        }>
+                        <Text>{ item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
