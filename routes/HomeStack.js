d050cf5d53c0cf139ca868a1012952875d4059bd
joynal67598 <59138477+joynalam67598@users.noreply.github.com>
2022-01-14 15:19:01 +0600

    navigation options

--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -5,13 +5,26 @@ import ReviewDetails from "../screens/reviewDetails"
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'Reviews',
+            // headerStyle:{backgroundColor:"blue"}
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+            // headerStyle:{backgroundColor:"green"}
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor:'#FFF',
+        headerStyle:{backgroundColor:"blue",height:70}
+    }
+});
 
 export default createAppContainer(HomeStack);
